047d749b5a87aad2305ea09f8ed32789b174ad59
damirusso95 <damirusso95@gmail.com>
2024-05-06 15:21:30 +0200

    M0: array con objet all'interno

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // CONSEGNA
 // Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
 // Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-// :roccia: MILESTONE 0:
+// MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite (in allegato zip).
 // :roccia: MILESTONE 1:
 // Stampare su console, per ogni membro del team: il nome e il ruolo.
@@ -9,3 +9,16 @@
 // Stampare in pagina, per ogni membro del team: nome, ruolo e immagine, come semplici stringhe.
 // Svolgere questa milestone grazie a una funzione può semplificarvi il lavoro per i bonus.
 //  La funzione riceve un oggetto e restituisce una stringa di HTML :occhiolino:
+
+// MILESTONE 0:
+let arrayOggetti = [
+     {nome1:"Wayne Barnett", ruolo:"Founder & CEO", foto: "wayne-barnett-founder-ceo.jpg"},
+     {nome2:"Angela Caroll", ruolo:"Chief Editor", foto: "angela-caroll-chief-editor.jpg"},
+     {nome3:"Walter Gordon", ruolo:"Office Manager", foto: "walter-gordon-office-manager.jpg"},
+     {nome4:"Angela Lopez", ruolo:"Social Media Manager", foto: "angela-lopez-social-media-manager.jpg"},
+     {nome5:"Scott Estrada", ruolo:"Developer", foto: "scott-estrada-developer.jpg"},
+     {nome6:"Barbara Ramos", ruolo:"Graphic Designer", foto: "barbara-ramos-graphic-designer.jpg"}
+
+
+];
+console.log(arrayOggetti)
