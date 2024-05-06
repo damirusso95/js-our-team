f10eb008806f5b6b5f992df41edac23b3ca22c1b
damirusso95 <damirusso95@gmail.com>
2024-05-06 15:51:34 +0200

    M1: ciclo for che stampa nome e ruolo per ogni oggetto dentro l'array

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // CONSEGNA
 // Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
 // Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-// MILESTONE 0:
-// Creare l’array di oggetti con le informazioni fornite (in allegato zip).
+
 // :roccia: MILESTONE 1:
 // Stampare su console, per ogni membro del team: il nome e il ruolo.
+
 // :roccia: MILESTONE 2:
 // Stampare in pagina, per ogni membro del team: nome, ruolo e immagine, come semplici stringhe.
 // Svolgere questa milestone grazie a una funzione può semplificarvi il lavoro per i bonus.
@@ -12,13 +12,27 @@
 
 // MILESTONE 0:
 let arrayOggetti = [
-     {nome1:"Wayne Barnett", ruolo:"Founder & CEO", foto: "wayne-barnett-founder-ceo.jpg"},
-     {nome2:"Angela Caroll", ruolo:"Chief Editor", foto: "angela-caroll-chief-editor.jpg"},
-     {nome3:"Walter Gordon", ruolo:"Office Manager", foto: "walter-gordon-office-manager.jpg"},
-     {nome4:"Angela Lopez", ruolo:"Social Media Manager", foto: "angela-lopez-social-media-manager.jpg"},
-     {nome5:"Scott Estrada", ruolo:"Developer", foto: "scott-estrada-developer.jpg"},
-     {nome6:"Barbara Ramos", ruolo:"Graphic Designer", foto: "barbara-ramos-graphic-designer.jpg"}
-
-
+    { nome: "Wayne Barnett", ruolo: "Founder & CEO", foto: "wayne-barnett-founder-ceo.jpg" },
+    { nome: "Angela Caroll", ruolo: "Chief Editor", foto: "angela-caroll-chief-editor.jpg" },
+    { nome: "Walter Gordon", ruolo: "Office Manager", foto: "walter-gordon-office-manager.jpg" },
+    { nome: "Angela Lopez", ruolo: "Social Media Manager", foto: "angela-lopez-social-media-manager.jpg" },
+    { nome: "Scott Estrada", ruolo: "Developer", foto: "scott-estrada-developer.jpg" },
+    { nome: "Barbara Ramos", ruolo: "Graphic Designer", foto: "barbara-ramos-graphic-designer.jpg" }
 ];
-console.log(arrayOggetti)
+// console.log(arrayOggetti);
+
+// MILESTONE 1:
+
+for (let i = 0; i < arrayOggetti.length; i++) {
+    const elemento = arrayOggetti[i]
+    // console.log(elemento);   
+
+    // for (let chiave in elemento) { 
+
+    // console.log(elemento.nome, elemento.ruolo);   
+    // console.log(elemento[chiave]);   
+    // }
+    console.log(arrayOggetti[i].nome, arrayOggetti[i].ruolo);
+}
+
+
